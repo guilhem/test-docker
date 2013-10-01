db99ec98d115fe2bcd2b7e3b85649da5ea79507d
Guilhem Lettron <guilhem@lettron.fr>
2013-10-01 22:07:24 +0200

    First Skeleton

---
+++ README.md
@@ -0,0 +1,13 @@
+# test-cookbook cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Guilhem Lettron (<guilhem@lettron.fr>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'test-cookbook'
+maintainer       'Guilhem Lettron'
+maintainer_email 'guilhem@lettron.fr'
+license          'Apache 2.0'
+description      'Installs/Configures test-cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
